ca8e0b63a3625abfadf6a19a8d955ee4fd438a93
Umesh Langade <umeshlangade5@gmail.com>
2022-07-01 20:14:12 +0530

    COMMIT

---
+++ Pattern7 - SquareShapeNumberReverseSeq.py
@@ -0,0 +1,22 @@
+# PATTERN STYLE
+
+# 333
+# 222
+# 111
+
+row = int(input('Kindly provide row limit : '))
+col = int(input('Kindly provide col limit : '))
+
+row += 1
+col += 1
+
+for i in range(row, 0, -1):
+    for j in range(col, 0, -1):
+        # print(f 'row-{i} column-{j}')
+        print(f'{j}', end="")
+    print()
+
+
+# REVERSE LOOP LOGIC
+# for i in range(row,-1,-1):
+#     print(f 'row {i}')
