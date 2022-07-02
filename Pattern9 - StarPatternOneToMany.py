3732695e0b9dbbba3123bef1d32d5c3d8dc6031b
Umesh Langade <umeshlangade5@gmail.com>
2022-07-02 12:40:31 +0530

    COMMIT AND PUSH

--- Pattern9 - StarPatternOneToMany.py
+++ Pattern9 - StarPatternOneToMany.py
@@ -4,10 +4,12 @@
 # **
 # ***
 # ****
+def pattern9():
+    row = int(input('Kindly provide row limit : '))
+    row += 1
+    col = row
+    for i in range(1, row):
+        print('*'*i)
 
-row = int(input('Kindly provide row limit : '))
-row += 1
-col = row
-
-for i in range(1, row):
-    print('*'*i)
+if __name__ == '__main__':
+    pattern9()
